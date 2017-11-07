ce2cfc0ec741e403f199fff89e4411f133034499
Andrew Adams <aadamsdev@gmail.com>
2017-11-07 11:40:08 -0500

    Inserting and wiping product table when new products are found, need to setup GCM

--- constants.js
+++ constants.js
@@ -2,5 +2,6 @@ module.exports = Object.freeze({
     NEW_PRODUCT = 'new_product',
     MODIFIED = 'product_modified',
     REMOVED = 'product_removed',
-    SOLD_OUT = 'sold_out'
+    SOLD_OUT = 'sold_out',
+    UNCHANGED = 'unchanged'
 })
--- database_scripts/selectAllProducts.js
+++ database_scripts/selectAllProducts.js
@@ -1,11 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/SeshMobileDatabase';
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  db.collection('products').find({}).toArray(function(err, result) {
+MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    console.log(result);
-    db.close();
-  });
+    db.collection('products').find({}).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
 });
